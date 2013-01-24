2508aaaf7337016333194daaf30a59e4b3bdd9d4
Guillaume Racicot <gufideg@gmail.com>
2013-01-24 10:15:14 -0500

    removed unused function

--- System/utilities.cpp
+++
@@ -1,9 +0,0 @@
-#include "utilities.h"
-
-std::string d2s(const double d)
-{
-	std::ostringstream strs;
-	strs << d;
-	return strs.str();
-}
-// kate: indent-mode cstyle; indent-width 4; replace-tabs off; tab-width 4; 
