b18293b0a712727e7e7afda376b13e70d45d63cd
Guillaume Racicot <gufideg@gmail.com>
2013-04-23 20:31:24 -0400

    removed unused sh script

--- deploy-dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -fr /usr/local/include/subgine
-rsync -av --include "*/" --include "*.h" --include "*.hpp" --exclude "*" --exclude "build/" --exclude ".git/" --exclude ".svn/" ~/prog/subgine/. /usr/local/include/subgine/
-#sed -i 's/\ \"/\ \<subgine\//g' /usr/local/include/subgine/*.hpp
-#sed -i 's/.h\"/.h\>/g' /usr/local/include/subgine/*.hpp
-#sed -i 's/.hpp\"/.hpp\>/g' /usr/local/include/subgine/*.hpp
