6d2c2483c8753f50f54b461c09d84d66834f5f6b
Guillaume Racicot <gufideg@gmail.com>
2013-06-09 02:03:35 -0400

    changed lists behaviour

--- src/Collision/Engines/collision.cpp
+++ src/Collision/Engines/collision.cpp
@@ -26,66 +26,86 @@ Collision::Collision() : _testers()
 
 Collision::~Collision()
 {
-	
+
 }
 
-void Collision::execute(const float time)
+void Collision::execute (const float time)
 {
-	for(auto it : _objects)
-	{
-		for(auto testMethod : it.second)
-		{
-			_testers[testMethod]->setAlias(testMethod);
-			_testers[testMethod]->addObject(*it.first, time);
+	for(auto test : _objects) {
+		
+	}
+}
+
+void Collision::add (Collisionnable::Collisionnable* object, std::list< std::string > groups)
+{
+	for (std::string & group : groups) {
+		_groups[group].push_back (object);
+	}
+
+	makeObjectList();
+}
+
+CollisionTester& Collision::getTester (const std::string tag)
+{
+	std::map<std::string, CollisionTester*>::iterator it = _testers.find (tag);
+
+	if (it != _testers.end()) {
+		return *it->second;
+	}
+
+	throw std::out_of_range ("No collision tester found with this tag");
+}
+
+const CollisionTester& Collision::getTester (const std::string tag) const
+{
+	std::map<std::string, CollisionTester*>::const_iterator it = _testers.find (tag);
+
+	if (it != _testers.end()) {
+		return *it->second;
+	}
+
+	throw std::out_of_range ("No collision tester found with this tag");
+}
+
+void Collision::addTester (CollisionTester* tester, const std::string tag)
+{
+	if (tester == nullptr || tester == 0) {
+		throw std::runtime_error ("The tester is null");
+	}
+
+	_testers[tag] = tester;
+	
+	makeObjectList();
+}
+
+void Collision::remove (Collisionnable::Collisionnable& object)
+{
+	for (auto group : _groups) {
+		group.second.remove (&object);
+	}
+
+	makeObjectList();
+}
+
+void Collision::remove (Collisionnable::Collisionnable& object, std::list< std::string > groups)
+{
+	for (auto group : groups) {
+		_groups[group].remove (&object);
+	}
+
+	makeObjectList();
+}
+
+void Collision::makeObjectList()
+{
+	_objects.clear();
+
+	for (auto groups : _groups) {
+		for (std::list<Collisionnable::Collisionnable*>::iterator item1 = groups.second.begin() ; item1 != groups.second.end() ; item1++) {
+			for (std::list<Collisionnable::Collisionnable*>::iterator item2 = item1 ; item2 != groups.second.end() ; item2++) {
+				_objects.push_back(std::make_tuple(_testers[groups.first], *item1, *item2));
+			}
 		}
 	}
-	
-	for(auto tester : _testers)
-	{
-		tester.second->flush();
-	}
-}
 
-void Collision::addData(Collisionnable::Collisionnable* object, std::list< std::string > testers)
-{	
-	_objects[object] = testers;
-}
-
-CollisionTester& Collision::getTester(const std::string tag)
-{
-	std::map<std::string, CollisionTester*>::iterator it = _testers.find(tag);
-
-	if(it != _testers.end())
-	{
-		return *it->second;
-	}
-
-	throw std::out_of_range("No collision tester found with this tag");
-}
-
-const CollisionTester& Collision::getTester(const std::string tag) const
-{
-	std::map<std::string, CollisionTester*>::const_iterator it = _testers.find(tag);
-
-	if(it != _testers.end())
-	{
-		return *it->second;
-	}
-
-	throw std::out_of_range("No collision tester found with this tag");
-}
-
-void Collision::addTester(CollisionTester* tester, const std::string tag)
-{
-	if(tester == nullptr || tester == 0)
-	{
-		throw std::runtime_error("The tester is null");
-	}
-	
-	_testers[tag] = tester;
-}
-
-void Collision::removeData(Collisionnable::Collisionnable& object)
-{
-	_objects.erase(&object);
 }
--- src/Collision/Engines/collision.h
+++ src/Collision/Engines/collision.h
@@ -18,10 +18,16 @@ public:
 	const CollisionTester& getTester(const std::string tag) const;
 	
 	void addTester(CollisionTester* tester, const std::string tag);
-	void addData(Collisionnable::Collisionnable* object, std::list< std::string > testers);
-	void removeData(Collisionnable::Collisionnable& object);
+	void add(Collisionnable::Collisionnable* object, std::list< std::string > groups);
+	void remove(Collisionnable::Collisionnable& object);
+	void remove(Collisionnable::Collisionnable& object, std::list< std::string > groups);
+	void clearAll();
 	
 protected:
-	std::map<Collisionnable::Collisionnable*, std::list<std::string>> _objects;
+	void makeObjectList();
+	
+	std::map<std::string, std::list<Collisionnable::Collisionnable*>> _groups;
 	std::map<std::string, CollisionTester*> _testers;
+	
+	std::vector<std::tuple<CollisionTester*, Collisionnable::Collisionnable*, Collisionnable::Collisionnable*>> _objects;
 };
