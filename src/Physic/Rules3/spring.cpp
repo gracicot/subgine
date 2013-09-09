c50d4c5ccd470e522f94be0959513c5a5b059fc7
Guillaume Racicot <gufideg@gmail.com>
2013-09-08 21:28:09 -0400

    spring3 accept a function as a position

--- src/Physic/Rules3/spring.cpp
+++ src/Physic/Rules3/spring.cpp
@@ -9,91 +9,74 @@ namespace physic
 namespace Rule3
 {
 
-Spring::Spring(const Vector3 value, const double size, Positionnable3& position) : _deletePosition(false), _value(value), _size(size)
-{
-	setPosition(position);
-}
-
-Spring::Spring(const Vector3 value, const double size, const Vector3 position) : _deletePosition(false), _value(value), _size(size)
-{
-	setPosition(position);
-}
-
-Spring::Spring(const Spring& other)
-{
-	if (_deletePosition) {
-		delete _position;
+	Spring::Spring(const subgine::Vector3 value, const double size, std::function< subgine::Vector3(void) > functor) : _value(value), _size(size), _functor(functor)
+	{
+		
 	}
-
-	_deletePosition = other._deletePosition;
-	_position = other._position;
-	_size = other._size;
-	_value = other._value;
-}
-
-Spring::~Spring()
-{
-	if (_deletePosition) {
-		delete _position;
+	
+	Spring::Spring(const Vector3 value, const double size, const Vector3 position) : _value(value), _size(size), _functor(nullptr), _position(position)
+	{
+		
 	}
-}
-
-Vector3 Spring::getPosition() const
-{
-	if (_position == nullptr) {
-		return Vector3();
+	
+	Spring::Spring(const Spring& other)
+	{
+		_functor = other._functor;
+		_position = other._position;
+		_size = other._size;
+		_value = other._value;
 	}
-
-	return _position->getPosition();
-}
-
-void Spring::setPosition(Positionnable3& position)
-{
-	if (_deletePosition) {
-		delete _position;
+	
+	Spring::~Spring()
+	{
+		
 	}
-
-	_deletePosition = false;
-	_position = &position;
-}
-
-void Spring::setPosition(Vector3 pos)
-{
-	if (_deletePosition) {
-		Vector3* position = dynamic_cast<Vector3*>(_position);
-		*position = pos;
-	} else {
-		_position = new Vector3(pos);
+	
+	Vector3 Spring::getPosition() const
+	{
+		if (_functor) {
+			return _functor();
+		} else {
+			return _position;
+		}
 	}
-
-	_deletePosition = true;
-}
-
-double Spring::getSize() const
-{
-	return _size;
-}
-
-void Spring::setSize(double size)
-{
-	_size = size;
-}
-
-Vector3 Spring::getResult(const PhysicPoint3& object) const
-{
-	Vector3 relative = getPosition() - object.getPosition();
-	return relative.unit() * getValue() * (relative.getLenght() - getSize());
-}
-
-Vector3 Spring::getValue() const
-{
-	return _value;
-}
-
-void Spring::setValue(const Vector3 value)
-{
-	_value = value;
-}
+	
+	void Spring::setPosition(std::function< subgine::Vector3(void) > functor)
+	{
+		_functor = functor;
+	}
+	
+	void Spring::setPosition(Vector3 pos)
+	{
+		_functor = nullptr;
+		_position = pos;
+	}
+	
+	double Spring::getSize() const
+	{
+		return _size;
+	}
+	
+	void Spring::setSize(const double size)
+	{
+		_size = size;
+	}
+	
+	Vector3 Spring::getResult(const PhysicPoint3& object) const
+	{
+		Vector3 relative = getPosition() - object.getPosition();
+		return relative.unit() * getValue() * (relative.getLenght() - getSize());
+	}
+	
+	Vector3 Spring::getValue() const
+	{
+		return _value;
+	}
+	
+	void Spring::setValue(const Vector3 value)
+	{
+		_value = value;
+	}
 
 }
 }
