6c6a451d8920f4c35879a4a25dda2415254a3126
Guillaume Racicot <gufideg@gmail.com>
2014-07-29 01:01:42 +0000

    renamed updatePhysic to update

--- src/Physic/abstractphysicpoint.h
+++ src/Physic/abstractphysicpoint.h
@@ -14,7 +14,7 @@ public:
 	void setMass(const double mass);
 	double getMass() const;
 	
-	virtual void updatePhysic(const double time) = 0;
+	virtual void update(const double time) = 0;
 
 protected:
 	double _mass;
