93234e3f49c9fd86473cef8c602947b99d9c245b
Guillaume Racicot <gufideg@gmail.com>
2014-07-29 00:58:09 +0000

    added default behaviour to aabb able class

--- src/Collision/aabb_able.cpp
+++ src/Collision/aabb_able.cpp
@@ -1,1 +1,74 @@
 #include "aabb_able.h"
+
+subgine::Vector2d subgine::collision::AABB_able::boxOverlap(const subgine::collision::AABB_able& other) const
+{
+	auto boundingBoxThis = getBoundingBox();
+	auto boundingBoxOther = other.getBoundingBox();
+
+	if (
+	    boundingBoxThis.second.x < boundingBoxOther.first.x ||
+	    boundingBoxThis.second.y < boundingBoxOther.first.y ||
+	    boundingBoxThis.first.x > boundingBoxOther.second.x ||
+	    boundingBoxThis.first.y > boundingBoxOther.second.y
+	) {
+		const Vector2& amin = boundingBoxThis.first;
+		const Vector2& amax = boundingBoxThis.second;
+		const Vector2& bmin = boundingBoxOther.first;
+		const Vector2& bmax = boundingBoxOther.second;
+
+		Vector2 mtd;
+
+		double left = (bmin.x - amax.x);
+		double right = (bmax.x - amin.x);
+		double top = (bmin.y - amax.y);
+		double bottom = (bmax.y - amin.y);
+
+		if (std::abs(left) < right) {
+			mtd.x = left;
+		} else {
+			mtd.x = right;
+		}
+
+		if (std::abs(top) < bottom) {
+			mtd.y = top;
+		} else {
+			mtd.y = bottom;
+		}
+
+		if (std::abs(mtd.x) < std::abs(mtd.y)) {
+			mtd.x = 0;
+		} else {
+			mtd.x = 0;
+		}
+
+		return mtd;
+	} else {
+		return Vector2d();
+	}
+}
+
+bool subgine::collision::AABB_able::isboxOverlapping(const subgine::collision::AABB_able& other) const
+{
+	auto boundingBoxThis = getBoundingBox();
+	auto boundingBoxOther = other.getBoundingBox();
+
+	return !(
+		boundingBoxThis.second.x < boundingBoxOther.first.x ||
+		boundingBoxThis.second.y < boundingBoxOther.first.y ||
+		boundingBoxThis.first.x > boundingBoxOther.second.x ||
+		boundingBoxThis.first.y > boundingBoxOther.second.y
+	);
+}
+
+bool subgine::collision::AABB_able::isContaining(const subgine::collision::AABB_able& other) const
+{
+	auto boundingBoxThis = getBoundingBox();
+	auto boundingBoxOther = other.getBoundingBox();
+	
+	return (
+		boundingBoxThis.first.x <= boundingBoxOther.first.x &&
+		boundingBoxThis.first.y <= boundingBoxOther.first.y &&
+		boundingBoxThis.second.x >= boundingBoxOther.second.x &&
+		boundingBoxThis.second.y >= boundingBoxOther.second.y 
+	);
+}
--- src/Collision/aabb_able.h
+++ src/Collision/aabb_able.h
@@ -14,8 +14,11 @@ class AABB_able : public virtual CollisionEntity
 public:
 	AABB_able() = default;
 	~AABB_able() = default;
-	virtual Vector2d boxOverlap(const AABB_able& other) const = 0;
+	virtual Vector2d boxOverlap(const AABB_able& other) const;
+	virtual bool isboxOverlapping(const AABB_able& other) const;
+	virtual bool isContaining(const AABB_able& other) const;
 	virtual std::pair<Vector2d, Vector2d> getBoundingBox() const = 0;
+	
 };
 
 }
