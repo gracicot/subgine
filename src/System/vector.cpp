d38b0348569decff21b9dce98975e64a7acdc99c
Guillaume Racicot <gufideg@gmail.com>
2014-11-06 00:42:12 +0000

    fixed compilation error with GCC

--- src/System/vector.cpp
+++ src/System/vector.cpp
@@ -3,6 +3,6 @@
 #include <cmath>
 
 namespace subgine {
-	constexpr const double pi = 4.0 * std::atan(1.0);
-	constexpr const double tau = 8.0 * std::atan(1.0);
+	const double pi = 4.0 * std::atan(1.0);
+	const double tau = 8.0 * std::atan(1.0);
 };
