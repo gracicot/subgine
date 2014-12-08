07a6ccb81588a7a0ffeb5494b4935f26d3996098
Guillaume Racicot <gufideg@gmail.com>
2014-12-08 03:08:31 +0000

    added function to return the number of degrees of freedom by dimensions

--- src/System/vector.h
+++ src/System/vector.h
@@ -5,6 +5,11 @@ namespace subgine
 	extern const double pi;
 	extern const double tau;
 	
+	constexpr int freedom(int n)
+	{
+		return n * (n - 1) / 2;
+	}
+	
 	template<int n, class T>
 	class Vector;
 }
