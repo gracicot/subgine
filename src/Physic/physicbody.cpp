a75cae6755a03b1142393b230c72db33bc7529d6
Guillaume Racicot <gufideg@gmail.com>
2014-12-08 21:34:25 +0000

    separated h to cpp for class physicbody

---
+++ src/Physic/physicbody.cpp
@@ -0,0 +1,229 @@
+#include "physicbody.h"
+
+#include "../System/Shape/shape.h"
+
+#include <cmath>
+#include <iostream>
+
+namespace subgine {
+namespace physic {
+
+template<int n>
+PhysicBody<n>::PhysicBody() : _torque(Angle()), _angularVelocity(Angle()), _orientation(Angle()) {}
+
+template<>
+void PhysicBody<2>::update(const double time)
+{
+	_orientation = std::fmod(getNextOrientation(time), tau);
+	_angularVelocity = getNextAngularVelocity(time);
+	_pulsesPosition.clear();
+	_torque = getNextTorque(time);
+
+	this->_position = PhysicPoint<2>::getNextPosition(time);
+	this->_corrections.clear();
+	this->_velocity = PhysicPoint<2>::getNextVelocity(time);
+	this->_pulses.clear();
+	this->_forces = PhysicPoint<2>::getNextForces();
+}
+
+template<>
+void PhysicBody<3>::update(const double time)
+{
+	Angle nextOrientation = getNextOrientation(time);
+	_orientation.x = std::fmod(nextOrientation.x, tau);
+	_orientation.y = std::fmod(nextOrientation.y, tau);
+	_orientation.z = std::fmod(nextOrientation.z, tau);
+	_angularVelocity = getNextAngularVelocity(time);
+	_pulsesPosition.clear();
+	_torque = getNextTorque(time);
+
+	this->_position = PhysicPoint<3>::getNextPosition(time);
+	this->_corrections.clear();
+	this->_velocity = PhysicPoint<3>::getNextVelocity(time);
+	this->_pulses.clear();
+	this->_forces = PhysicPoint<3>::getNextForces();
+}
+
+template<int n>
+Vector<n, double> PhysicBody<n>::getNextVelocity(const double time) const
+{
+	Vector<n, double> velocity = this->_velocity;
+
+	for (auto i : this->getNextForces()) {
+		velocity += (i.second / this->_mass) * time;
+	}
+
+	for (auto i : this->_pulses) {
+		Vector<n, double> pulse = i.second;
+		Vector<n, double> position = getPulsePosition(i.first);
+		double distance = position.getLength();
+
+		if (position.notZero() && pulse.notZero()) {
+			Angle torque = position.cross(pulse) / (distance * this->_mass);
+			pulse /= (torque.getLength() / getMomentOfInertia()) + 1;
+		}
+
+		velocity += (pulse / this->_mass);
+	}
+
+	return velocity;
+}
+
+template<int n>
+std::map<std::string, Vector<n, double>> PhysicBody<n>::getNextForces() const
+{
+	auto forces = this->_forces;
+
+	for (auto i : this->_rules) {
+		forces[i.first] = i.second->getResult(*this);
+	}
+
+	for (auto & i : forces) {
+		Vector<n, double> force = i.second;
+		Vector<n, double> position = getForcePosition(i.first);
+		double distance = position.getLength();
+
+		if (position.notZero() && force.notZero()) {
+			Angle torque = position.cross(force) / (distance * this->_mass);
+			i.second /= (torque.getLength() / getMomentOfInertia()) + 1;
+		}
+	}
+
+	return forces;
+}
+
+template<int n>
+Vector<freedom(n), double> PhysicBody<n>::getNextOrientation(const double time) const
+{
+	return _orientation + (getNextAngularVelocity(time)) * time;
+}
+
+template<int n>
+Vector<freedom(n), double> PhysicBody<n>::getOrientation() const
+{
+	return _orientation;
+}
+
+template<int n>
+Vector<freedom(n), double> PhysicBody<n>::getAngularVelocity() const
+{
+	return _angularVelocity;
+}
+
+template<int n>
+void PhysicBody<n>::setOrientation(Angle orientation)
+{
+	_orientation = orientation;
+}
+
+template<int n>
+Vector<freedom(n), double> PhysicBody<n>::getNextAngularVelocity(const double time) const
+{
+	Angle velocity = Angle();
+	Angle torquePulse = Angle();
+
+	velocity += (getNextTorque(time) / getMomentOfInertia()) * time;
+
+	for (auto forcePos : _pulsesPosition) {
+		Vector<n, double> force = this->getPulse(forcePos.first);
+		double distance = forcePos.second.getLength();
+
+		if (forcePos.second.notZero() && force.notZero()) {
+			Angle torque = forcePos.second.cross(force) / (distance * this->_mass);
+			torquePulse += torque;
+		}
+	}
+
+	velocity = torquePulse / getMomentOfInertia();
+
+	return _angularVelocity + (velocity / 1.0001) * tau;
+}
+
+template<int n>
+Vector<freedom(n), double> PhysicBody<n>::getTorque() const
+{
+	return _torque;
+}
+
+template<int n>
+Vector<freedom(n), double> PhysicBody<n>::getNextTorque(const double time) const
+{
+	Angle torque = Angle();
+
+	for (auto forcePos : _forcesPosition) {
+		Vector<n, double> force = this->getForce(forcePos.first);
+		double distance = forcePos.second.getLength();
+
+		Angle forceTorque = forcePos.second.cross(force) / (distance * this->_mass);
+		torque += forceTorque;
+	}
+
+	return torque;
+}
+
+template<int n>
+double PhysicBody<n>::getMomentOfInertia() const
+{
+	if (_shape) {
+		return _momentOfInertia;
+	} else {
+		return this->_mass;
+	}
+}
+
+template<int n>
+void PhysicBody<n>::setShape(std::shared_ptr<shape::Shape<n>> shape)
+{
+	_shape = shape;
+	_momentOfInertia = _shape->getMomentOfInertia(this->_mass);
+}
+
+template<int n>
+std::shared_ptr<shape::Shape<n>> PhysicBody<n>::getShape() const
+{
+	return _shape;
+}
+
+template<int n>
+void PhysicBody<n>::setPulse(const std::string type, const Vector<n, double> pulse, const Vector<n, double> position)
+{
+	this->_pulses[type] = pulse;
+	_pulsesPosition[type] = position;
+}
+
+template<int n>
+void PhysicBody<n>::setForce(const std::string type, const Vector<n, double> force, const Vector<n, double> position)
+{
+	this->_forces[type] = force;
+	_forcesPosition[type] = position;
+}
+
+template<int n>
+Vector<n, double> PhysicBody<n>::getPulsePosition(std::string type) const
+{
+	auto it = _pulsesPosition.find(type);
+
+	if (it != _pulsesPosition.end()) {
+		return it->second;
+	}
+
+	return Vector<n, double>();
+}
+
+template<int n>
+Vector<n, double> PhysicBody<n>::getForcePosition(std::string type) const
+{
+	auto it = _forcesPosition.find(type);
+
+	if (it != _forcesPosition.end()) {
+		return it->second;
+	}
+
+	return Vector<n, double>();
+}
+
+template class PhysicBody<2>;
+template class PhysicBody<3>;
+
+}
+}
--- src/Physic/physicbody.h
+++ src/Physic/physicbody.h
@@ -1,194 +1,58 @@
 #pragma once
 
 #include "physicpoint.h"
-#include "../System/Shape/shape.h"
 
-#include <iostream>
-#include <cmath>
 #include <memory>
 
 namespace subgine {
+
+namespace shape {
+	template<int n> class Shape;
+}
+
 namespace physic {
 
 template<int n>
 class PhysicBody : public PhysicPoint<n>
 {
 public:
-	typedef Vector<(n * (n - 1) / 2), double> Angle;
+	typedef Vector<freedom(n), double> Angle;
 	
-	PhysicBody() : _torque(Angle()), _angularVelocity(Angle()), _orientation(Angle()) {}
+	PhysicBody();
 
-	void update(const double time) override
-	{
-		_orientation = std::fmod(getNextOrientation(time), tau);
-		_angularVelocity = getNextAngularVelocity(time);
-		_pulsesPosition.clear();
-		_torque = getNextTorque(time);
-
-		this->_position = PhysicPoint<n>::getNextPosition(time);
-		this->_corrections.clear();
-		this->_velocity = PhysicPoint<n>::getNextVelocity(time);
-		this->_pulses.clear();
-		this->_forces = PhysicPoint<n>::getNextForces();
-	}
+	void update(const double time) override;
 	
-	virtual Vector<n, double> getNextVelocity(const double time) const override
-	{
-		Vector<n, double> velocity = this->_velocity;
-		
-		for (auto i : this->getNextForces()) {
-			velocity += (i.second / this->_mass) * time;
-		}
-		
-		for (auto i : this->_pulses) {
-			Vector<n, double> pulse = i.second;
-			Vector<n, double> position = getPulsePosition(i.first);
-			double distance = position.getLength();
-
-			if (position.notZero() && pulse.notZero()) {
-				Angle torque = position.cross(pulse) / (distance*this->_mass);
-				pulse /= (torque.getLength() / getMomentOfInertia()) + 1;
-			}
-			
-			velocity += (pulse / this->_mass);
-		}
-		
-		return velocity;
-	}
+	virtual Vector<n, double> getNextVelocity(const double time) const override;
 	
-	virtual std::map<std::string, Vector<n, double>> getNextForces() const
-	{
-		auto forces = this->_forces;
-		
-		for (auto i : this->_rules) {
-			forces[i.first] = i.second->getResult(*this);
-		}
-		
-		for (auto& i : forces) {
-			Vector<n, double> force = i.second;
-			Vector<n, double> position = getForcePosition(i.first);
-			double distance = position.getLength();
+	virtual std::map<std::string, Vector<n, double>> getNextForces() const;
 
-			if (position.notZero() && force.notZero()) {
-				Angle torque = position.cross(force) / (distance*this->_mass);
-				i.second /= (torque.getLength() / getMomentOfInertia()) + 1;
-			}
-		}
-		
-		return forces;
-	}
+	Angle getNextOrientation(const double time) const;
 
-	Angle getNextOrientation(const double time) const
-	{
-		return _orientation + (getNextAngularVelocity(time)) * time;
-	}
+	Angle getOrientation() const;
 
-	Angle getOrientation() const
-	{
-		return _orientation;
-	}
+	Angle getAngularVelocity() const;
 
-	Angle getAngularVelocity() const
-	{
-		return _angularVelocity;
-	}
+	void setOrientation(Angle orientation);
 
-	void setOrientation(Angle orientation)
-	{
-		_orientation = orientation;
-	}
+	Angle getNextAngularVelocity(const double time) const;
 
-	Angle getNextAngularVelocity(const double time) const
-	{
-		Angle velocity = Angle();
-		Angle torquePulse = Angle();
+	Angle getTorque() const;
 
-		velocity += (getNextTorque(time) / getMomentOfInertia()) * time;
+	Angle getNextTorque(const double time) const;
 
-		for (auto forcePos : _pulsesPosition) {
-			Vector<n, double> force = this->getPulse(forcePos.first);
-			double distance = forcePos.second.getLength();
+	double getMomentOfInertia() const;
 
-			if (forcePos.second.notZero() && force.notZero()) {
-				Angle torque = forcePos.second.cross(force) / (distance*this->_mass);
-				torquePulse += torque;
-			}
-		}
-
-		velocity = torquePulse / getMomentOfInertia();
-
-		return _angularVelocity + (velocity / 1.0001) * tau;
-	}
-
-	Angle getTorque() const
-	{
-		return _torque;
-	}
-
-	Angle getNextTorque(const double time) const
-	{
-		Angle torque = Angle();
-
-		for (auto forcePos : _forcesPosition) {
-			Vector<n, double> force = this->getForce(forcePos.first);
-			double distance = forcePos.second.getLength();
-
-			Angle forceTorque = forcePos.second.cross(force) / (distance*this->_mass);
-			torque += forceTorque;
-		}
-
-		return torque;
-	}
-
-	double getMomentOfInertia() const
-	{
-		if (_shape) {
-			return _momentOfInertia;
-		} else {
-			return this->_mass;
-		}
-	}
-
-	void setShape(std::shared_ptr<shape::Shape<n>> shape)
-	{
-		_shape = shape;
-		_momentOfInertia = _shape->getMomentOfInertia(this->_mass);
-	}
+	void setShape(std::shared_ptr<shape::Shape<n>> shape);
 	
-	std::shared_ptr<shape::Shape<n>> getShape() const
-	{
-		return _shape;
-	}
+	std::shared_ptr<shape::Shape<n>> getShape() const;
 
-	void setPulse(const std::string type, const Vector<n, double> pulse, const Vector<n, double> position = Vector<n, double>())
-	{
-		this->_pulses[type] = pulse;
-		_pulsesPosition[type] = position;
-	}
+	void setPulse(const std::string type, const Vector<n, double> pulse, const Vector<n, double> position = Vector<n, double>());
 
-	void setForce(const std::string type, const Vector<n, double> force, const Vector<n, double> position = Vector<n, double>())
-	{
-		this->_forces[type] = force;
-		_forcesPosition[type] = position;
-	}
+	void setForce(const std::string type, const Vector<n, double> force, const Vector<n, double> position = Vector<n, double>());
 	
-	Vector<n, double> getPulsePosition(std::string type) const
-	{
-		auto it = _pulsesPosition.find(type);
-		if (it != _pulsesPosition.end()) {
-			return it->second;
-		}
-		return Vector<n, double>();
-	}
+	Vector<n, double> getPulsePosition(std::string type) const;
 	
-	Vector<n, double> getForcePosition(std::string type) const
-	{
-		auto it = _forcesPosition.find(type);
-		if (it != _forcesPosition.end()) {
-			return it->second;
-		}
-		return Vector<n, double>();
-	}
+	Vector<n, double> getForcePosition(std::string type) const;
 
 protected:
 	double _momentOfInertia;
@@ -200,6 +64,12 @@ protected:
 	Angle _angularVelocity;
 	Angle _torque;
 };
+
+template <> void PhysicBody<2>::update(const double time);
+template <> void PhysicBody<3>::update(const double time);
+
+extern template class PhysicBody<2>;
+extern template class PhysicBody<3>;
 
 typedef PhysicBody<2> PhysicBody2;
 typedef PhysicBody<3> PhysicBody3;
