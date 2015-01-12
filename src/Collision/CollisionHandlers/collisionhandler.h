e8f9371b1a6446e436e174ecc724bcd72cad5c47
Guillaume Racicot <gufideg@gmail.com>
2015-01-12 12:49:42 +0000

    changed collision handler to the new collision body and result class

--- src/Collision/CollisionHandlers/collisionhandler.h
+++ src/Collision/CollisionHandlers/collisionhandler.h
@@ -1,20 +1,15 @@
 #pragma once
 
+#include "../CollisionResults/collisionresult.h"
 
-namespace sbg
-{
+namespace sbg {
 
 class CollisionBody;
-
-namespace Results {
-class CollisionResult;
-}
-
 
 class CollisionHandler
 {
 public:
-	virtual void apply(CollisionBody& object, const Results::CollisionResult& result) = 0;
+	virtual void apply(CollisionBody& object, CollisionResult result) = 0;
 	virtual CollisionHandler* clone() const = 0;
 };
 
