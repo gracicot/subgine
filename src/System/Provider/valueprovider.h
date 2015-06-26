27074e1cc507eadb69b83035b922a7ff384130d2
Guillaume Racicot <gufideg@gmail.com>
2015-06-26 15:44:29 -0400

    better value provider constructor

--- src/System/Provider/valueprovider.h
+++ src/System/Provider/valueprovider.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include <functional>
+#include <type_traits>
 
 namespace sbg {
 
 template<typename T>
 struct ValueProvider final {
-	ValueProvider(std::function<T()> callback) : _callback{callback} {}
-	ValueProvider(T value) : _callback{[=]{ return value; }} {}
+	template<typename U, typename = typename std::enable_if<std::is_constructible<std::function<T()>, U>::value>::type>
+	ValueProvider(U&& callback) : _callback{std::forward<U>(callback)} {}
+	ValueProvider(typename std::enable_if<!std::is_constructible<std::function<T()>, T>::value, T>::type value) : _callback{[=]{ return value; }} {}
 	
 	T operator()() const {
 		return _callback();
