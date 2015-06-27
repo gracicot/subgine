250f1eaf464aba5453d82775f164d5851d97b8f7
Guillaume Racicot <gufideg@gmail.com>
2015-06-27 13:43:50 +0000

    added template copy and move constructor

--- src/System/Provider/valueprovider.h
+++ src/System/Provider/valueprovider.h
@@ -13,15 +13,26 @@ struct ValueProvider final {
 	
 	ValueProvider& operator=(ValueProvider&& other) {
 		std::swap(other._callback, _callback);
+		return *this;
 	}
 	
 	ValueProvider& operator=(const ValueProvider& other) {
 		_callback = other._callback;
+		return *this;
 	}
 	
-	template<typename U, typename = typename std::enable_if<std::is_constructible<std::function<T()>, U>::value>::type>
+	template<typename U, typename = typename std::enable_if<std::is_convertible<U, T>::value>::type>
+	ValueProvider(const ValueProvider<U>& other) : _callback{other._callback} {}
+	
+	template<typename U, typename = typename std::enable_if<std::is_convertible<U, T>::value>::type>
+	ValueProvider(ValueProvider<U>&& other) : _callback{std::move(other._callback)} {}
+	
+	template<typename U, typename = typename std::enable_if<std::is_constructible<std::function<T()>>::value>::type>
 	ValueProvider(U callback) : _callback{callback} {}
 	ValueProvider(typename std::enable_if<!std::is_constructible<std::function<T()>, T>::value, T>::type value) : _callback{[=]{ return value; }} {}
+	
+	template<typename U>
+	friend struct ValueProvider;
 	
 	T operator()() const {
 		return _callback();
