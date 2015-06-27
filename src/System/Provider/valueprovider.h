b11cbeab90bd2fb75727f46a357557e260c2e830
Guillaume Racicot <gufideg@gmail.com>
2015-06-27 02:49:20 +0000

    better ctors

--- src/System/Provider/valueprovider.h
+++ src/System/Provider/valueprovider.h
@@ -7,8 +7,20 @@ namespace sbg {
 
 template<typename T>
 struct ValueProvider final {
+	ValueProvider() = delete;
+	ValueProvider(const ValueProvider& other) : _callback{other._callback} {}
+	ValueProvider(ValueProvider&& other) : _callback{std::move(other._callback)} {}
+	
+	ValueProvider& operator=(ValueProvider&& other) {
+		std::swap(other._callback, _callback);
+	}
+	
+	ValueProvider& operator=(const ValueProvider& other) {
+		_callback = other._callback;
+	}
+	
 	template<typename U, typename = typename std::enable_if<std::is_constructible<std::function<T()>, U>::value>::type>
-	ValueProvider(U&& callback) : _callback{std::forward<U>(callback)} {}
+	ValueProvider(U callback) : _callback{callback} {}
 	ValueProvider(typename std::enable_if<!std::is_constructible<std::function<T()>, T>::value, T>::type value) : _callback{[=]{ return value; }} {}
 	
 	T operator()() const {
